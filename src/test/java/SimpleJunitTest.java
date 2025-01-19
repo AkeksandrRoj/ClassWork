661f191c928c55b9ac57b32f97c64fcc68964d69
Akeksandr Rojkov <dir.gefest@gmail.com>
2025-01-19 20:50:59 +0300

    add JUnit5

---
+++ src/test/java/SimpleJunitTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.*;
+
+public class SimpleJunitTest {
+
+    int result;
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("### - beforeAll\n");
+    }
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        System.out.println("### BeforeEach");
+         result =getResult();
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###     firstTest");
+        Assertions.assertTrue(result>3);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###     secondTest");
+        Assertions.assertTrue(result>3);
+
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###     thirdTest");
+        Assertions.assertTrue(result>3);
+
+    }
+
+    int getResult() {
+        return 5;
+    }
+
+    @AfterEach
+    void afterEach() throws Exception {
+        System.out.println("###         AfterEach\n");
+        result = 0;
+    }
+
+}
